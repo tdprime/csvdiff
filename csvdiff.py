11a77bfc795c3edb8c3fced1a08a2bd2cead80f8
Timothy Prime <tdprime@fastmail.fm>
2021-10-31 15:01:32 -0400

    highlight column changes within rows
    Creating a new diff format requires difflib.SequenceMatcher to
    expose details about each set of differences.  Model csvdiff()
    similar to ndiff().
    
    In preparation for a custom diff, simplified the CSV input.  Replaced
    the string rows with tuple().  SequenceMatcher requires a hashable
    type.  Using tuples makes field comparisons reasonably easy.
    
    Most of the attention is on replaced rows.  The data is compared
    column by column.  Each differing pair highlighted as needed.
    
    When the data compared is similar, getting good results from a super
    simple greedy algorithm.  The more fancy parts of ndiff() will need
    to be done later.

--- csvdiff.py
+++ csvdiff.py
@@ -3,6 +3,7 @@
 import csv
 import difflib
 import io
+import operator
 import signal
 import sys
 
@@ -15,6 +16,87 @@ class ResetStringIO(io.StringIO):
 		finally:
 			self.seek(0)
 			self.truncate()
+
+FMT_EQUAL  = lambda x: x
+TOK_EQUAL  = ' '
+FMT_DELETE = lambda x: termcolor.colored(x, 'red')
+TOK_DELETE = FMT_DELETE('-')
+FMT_INSERT = lambda x: termcolor.colored(x, 'green')
+TOK_INSERT = FMT_INSERT('+')
+
+# TODO: see difflib.context_diff
+def csvdiff(a, b):
+	csvout = csv.writer(sys.stdout)
+	cruncher = difflib.SequenceMatcher(None, a, b)
+	for tag, alo, ahi, blo, bhi in cruncher.get_opcodes():
+		# Order by expected cardinality. Most frequent first.
+		if tag == 'equal':
+			pass
+		elif tag == 'replace':
+			csvreplace(a, b, alo, ahi, blo, bhi)
+		elif tag == 'insert':
+			for row in b[blo:bhi]:
+				sys.stdout.write(TOK_INSERT)
+				csvout.writerow(list(map(FMT_INSERT, row)))
+		elif tag == 'delete':
+			for row in a[alo:ahi]:
+				sys.stdout.write(TOK_DELETE)
+				csvout.writerow(list(map(FMT_DELETE, row)))
+
+# Super simple. Best for ranges with the same length.
+def csvreplace(a, b, alo, ahi, blo, bhi):
+	csvout = csv.writer(sys.stdout)
+	alen = ahi - alo
+	blen = bhi - blo
+	i, j = alo, blo
+	while alen > blen:
+		sys.stdout.write(TOK_DELETE)
+		csvout.writerow(list(map(FMT_DELETE, a[i])))
+		i += 1
+		alen -= 1
+
+	while alen > 0:
+		rowa = list()
+		rowb = list()
+		for x,y in zip(a[i], b[j]):
+			if x == y:
+				rowa.append(x)
+				rowb.append(y)
+			else:
+				rowa.append(FMT_DELETE(x))
+				rowb.append(FMT_INSERT(y))
+		sys.stdout.write(TOK_DELETE)
+		csvout.writerow(rowa)
+		sys.stdout.write(TOK_INSERT)
+		csvout.writerow(rowb)
+		i += 1
+		j += 1
+		alen -= 1
+		blen -= 1
+
+	while blen > 0:
+		sys.stdout.write(TOK_INSERT)
+		csvout.writerow(list(map(FMT_INSERT, b[j])))
+		j += 1
+		blen -= 1
+
+# count matching columns per pair of rows
+def rowcompare(a, b):
+	score = 0
+	for x,y in zip(a, b):
+		score += int(x == y)
+	return score
+
+# Inspired by ndiff. Take advantage of the data's structure to better inform
+# how they compare.
+def _csvreplace(a, b, alo, ahi, blo, bhi):
+	cutoff = .75 * len(a[0])
+	score = dict()
+	for j in range(blo, bhi):
+		for i in range(alo, ahi):
+			score[(i,j)] = rowcompare(a[i], b[j])
+			if best[0] < score[(i,j)]:
+				best = score[(i,j)], i, j
 
 def diffprint(line, **kwargs):
 	if line[0] == ' ':
@@ -31,38 +113,25 @@ def diffprint(line, **kwargs):
 		termcolor.cprint(line, attrs=['reverse'], **kwargs)
 
 if __name__ == '__main__':
+	signal.signal(signal.SIGPIPE, signal.SIG_DFL)
+
 	with open(sys.argv[1]) as in1, open(sys.argv[2]) as in2:
 		csv1 = csv.DictReader(in1)
 		csv2 = csv.DictReader(in2)
 
+		# schema is the set union of the two CSV field names.
+		# Keep the order of the "new" CSV.
 		schema = set(csv1.fieldnames)
-		schema = list(filter(lambda x: x in schema, csv2.fieldnames))
+		schema = tuple(filter(lambda x: x in schema, csv2.fieldnames))
 		# print('DEBUG: schema = [%s]' % (','.join(schema)))
-		csvout = ResetStringIO()
-		csvw = csv.DictWriter(csvout, fieldnames=schema, extrasaction='ignore')
+		# Using tuple as a hashable (immutable) Row type.
+		hrow = operator.itemgetter(*schema)
+		data1 = list(map(hrow, csv1))
+		data2 = list(map(hrow, csv2))
 
-		data1 = list()
-		for row in csv1:
-			csvw.writerow(row)
-			row = csvout.getvalue()
-			# sys.stdout.write('DEBUG: ' + row)
-			data1.append(row)
-		# print('DEBUG: %s has %d rows' % (sys.argv[1], len(data1)))
-
-		data2 = list()
-		for row in csv2:
-			csvw.writerow(row)
-			data2.append(csvout.getvalue())
-		# print('DEBUG: %s has %d rows' % (sys.argv[2], len(data2)))
-
-	signal.signal(signal.SIGPIPE, signal.SIG_DFL)
 	print('Schema:')
 	for x in difflib.ndiff(csv1.fieldnames, csv2.fieldnames):
 		if x[0] != '?':
 			diffprint(x)
 	print('\nRows:')
-	#for x in difflib.unified_diff(data1, data2, n=0,
-	#	fromfile=sys.argv[1], tofile=sys.argv[2]):
-	for x in difflib.ndiff(data1, data2):
-		if x[0] != ' ':
-			diffprint(x, end='')
+	csvdiff(data1, data2)
